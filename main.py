461f556d112cf1b8973ffb00dfdef25bb80d2579
asjnsb <asjnsb@gmail.com>
2023-05-12 13:52:46 -0400

    imgComp working

--- main.py
+++ main.py
@@ -48,3 +48,14 @@ def mse(img1, img2):
     err = np.sum(diff**2)
     mse = err/(float(h*w))
     return mse
+
+img0 = cv2.cvtColor(cv2.imread('heartless.jpg'), cv2.COLOR_BGR2GRAY)
+img1 = cv2.cvtColor(cv2.imread('hearts.jpg'), cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(cv2.imread('Beach.jpg'), cv2.COLOR_BGR2GRAY)
+
+
+error = mse(img0, img1)
+error1 = mse(img0, img2)
+
+print("Image matching Error between img0 & img1:", error)
+print("Image matching Error between img0 & img2:", error1)
