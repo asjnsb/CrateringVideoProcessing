94edf92c8eaebe787f654618a73ff12f3bc9f1e6
asjnsb <asjnsb@gmail.com>
2023-06-30 16:01:56 -0400

    end of day

--- directorytester.py
+++ directorytester.py
@@ -4,3 +4,4 @@ Dir = "directorytesterTESTDIR"
 
 absPath = os.path.dirname(__file__)
 print(absPath)
+#NEXT: https://docs.python.org/3/library/os.path.html for docs: os.path.join os.path.relpath os.path.split should be useful
