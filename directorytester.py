e648f097ddada06c87908be81d3b012f5eafd86d
asjnsb <asjnsb@gmail.com>
2023-07-07 15:57:29 -0400

    EoD

--- directorytester.py
+++ directorytester.py
@@ -1,7 +1,12 @@
 import os
 
-Dir = "directorytesterTESTDIR"
-
 absPath = os.path.dirname(__file__)
-print(absPath)
-#NEXT: https://docs.python.org/3/library/os.path.html for docs: os.path.join os.path.relpath os.path.split should be useful
+head, tail = os.path.split(absPath)
+while "Users" not in os.path.basename(head):
+    head, tail = os.path.split(head)
+
+
+print(head)
+
+#https://docs.python.org/3/library/os.path.html for docs: os.path.join os.path.relpath os.path.split should be useful
+#NEXT: split works great, just keep working
