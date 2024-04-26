273680d0896541a9351c3ca577eab8a605461fd0
asjnsb <asjnsb@gmail.com>
2024-04-26 12:44:21 -0400

    EOD

--- Image processing.py
+++ Image processing.py
@@ -27,7 +27,7 @@ threshold2 = 70
 testLim = 0
 frameLim = 0
 # Start from a particular test
-testStart = 128
+testStart = 232
 #=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
 
 def imgProcessor(imgPath):
